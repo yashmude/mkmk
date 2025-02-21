2aa398b275f8e2f160977aeec91ab9456be1b92c
yashmude <yashmude01@gmail.com>
2025-02-21 22:12:08 +0530

    .

---
+++ connection.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "sql12.freemysqlhosting.net";
+$username = "sql12708977";
+$password = "JG38N4Yxa1";
+$dbname= "sql12708977";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
